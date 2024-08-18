951dec063bef6aaeacbee0462d06d2900501cbb5
muriisantuss <muriisantuss@gmail.com>
2024-08-18 16:09:48 -0300

    wip(menu): part final of the price calculation implementation

--- js.js
+++ js.js
@@ -7,26 +7,33 @@ var products = [
   { id: 6, name: "Bife com batata", price: 12.0 },
 ];
 
-const formatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-})
+const formatter = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
 
 function calc() {
   output = document.getElementById("output");
-  const quantities = document.getElementsByName("quantity")
+  const quantities = document.getElementsByName("quantity");
+  var finalPrice = document.getElementsByClassName("finalPrice")[0];
   output.innerHTML = "";
-  var total = 0
+  var total = 0;
 
-  for(var input of quantities){
-    const id = input.id
-    output.innerHTML += `Caro</br>`;
-    output.innerHTML += `Segue os dados do seu pedido</br>`;
+  output.innerHTML += `Caro(a)</br>`;
+  output.innerHTML += `Segue os dados do seu pedido:</br></br>`;
 
-    output.innerHTML += `Produto: ${products[id-1].name} - Preço: ${formatter.format(products[id-1].price)} - Quantidade: ${input.value} </br>`;
-    total += formatter.format(products[id-1].price) * parseInt(input.value)
+  output.innerHTML += `<ul id="orderList"></ul>`;
+  const orderList = document.getElementById("orderList");
 
+  for (var input of quantities) {
+    const id = input.id;
+
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Prato: ${products[id-1].name} - Preço: ${formatter.format(products[id-1].price)} - Quantidade: ${input.value}`;
+
+    orderList.appendChild(listItem);
+    total += products[id - 1].price * parseFloat(input.value);
+    console.log(total);
   }
-  output.innerHTML += `Preço Final: ${total}</br></br>`;
-
+  finalPrice.innerHTML = `Preço Final: ${formatter.format(total)} </br></br>`;
 }
